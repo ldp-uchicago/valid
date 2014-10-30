4b452c9dc352805706c124f4263a9632d55d1b24
moniker001 <lim.tejong@gmail.com>
2014-10-30 15:35:28 -0500

    added gtype validation tests

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var test = require('tape'),
-    check = require('schema');
+    check = require('./schema');
 
 
 test('schema', function (t) {
@@ -73,33 +73,101 @@ test('schema', function (t) {
             'Time = `0` required format is HH:MM:SS',
             "ensure required format"
         );
-        /*
-        t.equal(check.Time('00:00'), undefined, "check for hours, minutes, and seconds");
-        t.equal(check.Time('1:01:01'), undefined, "permit only double digit for hours");
-        t.equal(check.Time('01:1:01'), undefined, "permit only double digit for minutes");
-        t.equal(check.Time('01:01:1'), undefined, "permit only double digit for seconds");
-        */
+        t.equal(
+            check.Time('00:00'),
+            'Time = `00:00` required format is HH:MM:SS',
+            "ensure required format"
+        );
+        t.equal(
+            check.Time('1:01:01'),
+            'Time = `1:01:01` required format is HH:MM:SS',
+            "ensure required format"
+        );
+        t.equal(
+            check.Time('01:1:01'),
+            'Time = `01:1:01` required format is HH:MM:SS',
+            "ensure required format"
+        );
+        t.equal(
+            check.Time('01:01:1'),
+            'Time = `01:01:1` required format is HH:MM:SS',
+            "ensure required format"
+        );
+
+        t.end();
+    });
+    
+    test('G-type column constraints', function (t) {
+        t.equal(check.Gtype(''), undefined, "permit empty strings");
+        t.equal(check.Gtype('C'), undefined);
+        t.equal(check.Gtype('DP'), undefined);
+        t.equal(check.Gtype('C/DP'), undefined);
+        t.equal(check.Gtype('C;DP'), undefined);
+        t.equal(
+            check.Gtype('A'),
+            'Gtype = `A` is an invalid entry',
+            "check for valid characters"
+        );
+        t.equal(
+            check.Gtype('C/DP '),
+            'Gtype = `C/DP ` entry cannot contain space',
+            "check for spaces"
+        );
+        t.equal(
+            check.Gtype(';C/DP'),
+            'Gtype = `;C/DP` entry cannot begin with ;',
+            "check beginning for ;"
+        );
+        t.equal(
+            check.Gtype('/C/DP'),
+            'Gtype = `/C/DP` entry cannot begin with /',
+            "check beginning for /"
+        );
+        t.equal(
+            check.Gtype('C/DP;'),
+            'Gtype = `C/DP;` entry cannot end with ;',
+            "check end for ;"
+        );
+        t.equal(
+            check.Gtype('C/DP/'),
+            'Gtype = `C/DP/` entry cannot end with /',
+            "check end for /"
+        );
+        t.equal(
+            check.Gtype('C/43/DP'),
+            'Gtype = `C/43/DP` entry cannot contain numeric characters',
+            "check for numbers"
+        );
+        t.equal(
+            check.Gtype('C//DP'),
+            'Gtype = `C//DP` entry cannot contain //',
+            "check for //"
+        );
+        t.equal(
+            check.Gtype('C;;DP'),
+            'Gtype = `C;;DP` entry cannot contain ;;',
+            "check for ;;"
+        );
         t.end();
     });
 
     t.end();
 });
 
-
 test('validate', function (t) {
 
     var records = [
-            {"_ID": "22", "ROW": "1", "LRB": "L", "XYZ": "x"},
-            {"_ID": "22", "ROW": "2", "LRB": "L+L", "XYZ": "y"},
-            {"_ID": "22", "ROW": "3", "LRB": "L+ ", "XYZ": "z"},
-            {"_ID": "22", "ROW": "4", "LRB": "L+R+B", "XYZ": "q"},
-            {"_ID": "22", "ROW": "5", "LRB": "L+R+X", "XYZ": "b"}
+            {"_ID": "22", "ROW": "1", "LRB": "L", "XYZ": "x", "Time": "00:00:00", "Gtype": "C"},
+            {"_ID": "22", "ROW": "2", "LRB": "L+L", "XYZ": "y", "Time": "00:00:00", "Gtype": "C"},
+            {"_ID": "22", "ROW": "3", "LRB": "L+ ", "XYZ": "z", "Time": " 30:00:00", "Gtype": "C"},
+            {"_ID": "22", "ROW": "4", "LRB": "L+R+B", "XYZ": "q", "Time": "23:59:59", "Gtype": "C"},
+            {"_ID": "22", "ROW": "5", "LRB": "L+R+X", "XYZ": "b", "Time": "00:0:00", "Gtype": "C"}
         ],
         Validator = require('valid-records'),
         valid = new Validator(check),
         results = valid.validate(records);
 
     t.plan(2);
-    t.equal(results.report.invalid, 2);
-    t.equal(Object.keys(results.report.errors).length, 2, "2 lines have errors");
+    t.equal(results.report.invalid, 9);
+    t.equal(Object.keys(results.report.errors).length, 5, "5 lines have errors");
 });
