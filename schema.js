467db69009573afcba61a367c24101bdaaddf1d8
moniker001 <lim.tejong@gmail.com>
2014-11-12 15:36:37 -0600

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,4 +1,5 @@
 'use strict';
+var patterns = require('./patterns');
 
 
 // helper function to format reply for invalid values
@@ -20,7 +21,9 @@ var invalidReply = function (col, val, reason) {
 //
 var lrb = function (v) {
 
-    var column = 'LRB';
+    var column = 'lrb';
+
+    // has a value
     if (v) {
         if (!/^[LRB](?:\+[LRB])*$/.test(v)) {
             return invalidReply(column, v, 'is an invalid value');
@@ -33,7 +36,8 @@ var lrb = function (v) {
 //
 var time = function (v) {
 
-    var column = 'Time';
+    var column = 'time';
+
     // has a value
     if (v) {
         // is in the general format
@@ -57,15 +61,15 @@ var time = function (v) {
             }
 
             if (/^\d:/.test(v)) {
-                return invalidReply(column, v, 'required format is HH:MM:SS');
+                return invalidReply(column, v, 'need two digits for hours');
             }
 
             if (/^\d\d:\d:/.test(v)) {
-                return invalidReply(column, v, 'required format is HH:MM:SS');
+                return invalidReply(column, v, 'need two digits for minutes');
             }
 
             if (/:\d$/.test(v)) {
-                return invalidReply(column, v, 'required format is HH:MM:SS');
+                return invalidReply(column, v, 'need two digits for seconds');
             }
 
             return invalidReply(column, v, 'required format is HH:MM:SS');
@@ -74,43 +78,57 @@ var time = function (v) {
 };
 
 var gtype = function (v) {
-// (?:(\/|;)(C|DP|DP.nl|DS|DSDP|E|FA|G|S|R.a|R.d|R.m|R.a.pp|R.d.pp|R.m.pp|R.a.e|R.d.e|R.m.e|R.met))*$/
-    var column = 'Gtype';
+
+    var column = 'gtype';
+
+    // has a value
     if (v) {
-        if (!/^(C|DP|DP\.nl|DS|DSDP|E|FA|G|S|R\.a|R\.d|R\.m|R\.a\.pp|R\.d\.pp|R\.m\.pp|R\.a\.e|R\.d\.e|R\.m\.e|R\.met)((\/|;)(C|DP|DP\.nl|DS|DSDP|E|FA|G|S|R\.a|R\.d|R\.m|R\.a\.pp|R\.d\.pp|R\.m\.pp|R\.a\.e|R\.d\.e|R\.m\.e|R\.met))*$/.test(v)) {
+        if (!patterns.gtype.test(v)) {
+
             if (/\s/.test(v)) {
                 return invalidReply(column, v, 'entry cannot contain space');
             }
+
             if (/^;/.test(v)) {
                 return invalidReply(column, v, 'entry cannot begin with ;');
             }
+
             if (/^\//.test(v)) {
                 return invalidReply(column, v, 'entry cannot begin with /');
             }
+
             if (/^\./.test(v)) {
                 return invalidReply(column, v, 'entry cannot begin with .');
             }
+
             if (/;$/.test(v)) {
                 return invalidReply(column, v, 'entry cannot end with ;');
             }
+
             if (/\/$/.test(v)) {
                 return invalidReply(column, v, 'entry cannot end with /');
             }
+
             if (/\.$/.test(v)) {
                 return invalidReply(column, v, 'entry cannot end with .');
             }
+
             if (/[0-9]/.test(v)) {
-                return invalidReply(column, v, 'entry cannot contain numeric characters');
+                return invalidReply(column, v, 'entry cannot contain numbers');
             }
+
             if (/(\/\/)/.test(v)) {
                 return invalidReply(column, v, 'entry cannot contain //');
             }
+
             if (/;;/.test(v)) {
                 return invalidReply(column, v, 'entry cannot contain ;;');
             }
+
             if (/(\.\.)/.test(v)) {
                 return invalidReply(column, v, 'entry cannot contain ..');
             }
+
             return invalidReply(column, v, 'is an invalid entry');
         }
     }
@@ -118,41 +136,160 @@ var gtype = function (v) {
 
 var gsrel = function (v) {
 
-    var column = 'GSRel';
+    var column = 'gsrel';
     if (v) {
-        if (!/^(DA|MS|UC|X|ADD|ADD.err|ADD.err.s|ADD.f|ADD.met|ADD.nr|ADD.ns|ADD.q|ADD.s|E|E.b|RF|RF.a|RF.p)((\/|;)(DA|MS|UC|X|ADD|ADD.err|ADD.err.s|ADD.f|ADD.met|ADD.nr|ADD.ns|ADD.q|ADD.s|E|E.b|RF|RF.a|RF.p))*$/.test(v)) {
+        if (!patterns.gsrel.test(v)) {
+
             if (/\s/.test(v)) {
                 return invalidReply(column, v, 'entry cannot contain space');
             }
+
             if (/^;/.test(v)) {
                 return invalidReply(column, v, 'entry cannot begin with ;');
             }
+
             if (/^\//.test(v)) {
                 return invalidReply(column, v, 'entry cannot begin with /');
             }
+
             if (/^\./.test(v)) {
                 return invalidReply(column, v, 'entry cannot begin with .');
             }
+
             if (/;$/.test(v)) {
                 return invalidReply(column, v, 'entry cannot end with ;');
             }
+
             if (/\/$/.test(v)) {
                 return invalidReply(column, v, 'entry cannot end with /');
             }
+
             if (/\.$/.test(v)) {
                 return invalidReply(column, v, 'entry cannot end with .');
             }
+
             if (/[0-9]/.test(v)) {
-                return invalidReply(column, v, 'entry cannot contain numeric characters');
+                return invalidReply(column, v, 'entry cannot contain numbers');
             }
+
             if (/(\/\/)/.test(v)) {
                 return invalidReply(column, v, 'entry cannot contain //');
             }
+
             if (/;;/.test(v)) {
                 return invalidReply(column, v, 'entry cannot contain ;;');
             }
+
             if (/(\.\.)/.test(v)) {
                 return invalidReply(column, v, 'entry cannot contain ..');
+            }
+
+            return invalidReply(column, v, 'is an invalid entry');
+        }
+    }
+};
+
+var key = function (v) {
+
+    var column = 'Key';
+    if (v) {
+        if (!/^([1-9adefFilmMoprtvx]|xc)([1-9adefFilmMoprtvx]|xc)*$/.test(v)) {
+
+            if (/\s/.test(v)) {
+                return invalidReply(column, v, 'entry cannot contain space');
+            }
+
+            if (/0/.test(v)) {
+                return invalidReply(column, v, 'entry cannot contain 0');
+            }
+
+            return invalidReply(column, v, 'is an invalid entry');
+        }
+    }
+};
+
+var utts = function (v) {
+
+    var column = 'Utts';
+    if (v) {
+        if (/^[\-#@a-z\s][\-#@a-z\s]*$/.test(v)) {
+
+            if (/^\s/.test(v)) {
+                return invalidReply(column, v, 'entry cannot begin with space');
+            }
+
+            if (/\s$/.test(v)) {
+                return invalidReply(column, v, 'entry cannot end with space');
+            }
+
+            if (/\-/.test(v)) {
+                if (/---/.test(v)) {
+                    if (/----/.test(v)) {
+                        return invalidReply(column, v, 'entry can only contain ---');
+                    }
+
+                } else {
+                    return invalidReply(column, v, 'entry cannot contain - or --');
+                }
+            }
+
+            if (/[#]/.test(v)) {
+                if (/###/.test(v)) {
+                    if (/####/.test(v)) {
+                        return invalidReply(column, v, 'entry can only contain ###');
+                    }
+
+                } else {
+                    return invalidReply(column, v, 'entry cannot contain # or ##');
+                }
+            }
+
+            if (/@/.test(v)) {
+                if (!/@f/.test(v) && !/@l/.test(v)) {
+                    return invalidReply(column, v, "entry can only have @f or @l");
+                }
+
+                if (/@f/.test(v)) {
+                    if (/\s/.test(v)) {
+                        if (!/@f\s/.test(v) && !/@f$/.test(v)) {
+                            return invalidReply(column, v, '@f must be at end of word');
+                        }
+                    } else {
+                        if (!/@f$/.test(v)) {
+                            return invalidReply(column, v, '@f must be at end of word');
+                        }
+                    }
+                }
+
+                if (/@l/.test(v)) {
+                    if (/\s/.test(v)) {
+                        if (!/@l\s/.test(v) && !/@l$/.test(v)) {
+                            return invalidReply(column, v, '@l must be at end of word');
+                        }
+                        if (!/\s@l/.test(v) && !/^@l/.test(v)) {
+                            return invalidReply(column, v, '@l must be at beginning of word');
+                        }
+                    } else {
+                        if (!/@l$/.test(v)) {
+                            return invalidReply(column, v, '@l must be at end of word');
+                        }
+                        if (!/^@l/.test(v)) {
+                            return invalidReply(column, v, '@l must be at beginning of word');
+                        }
+                    }
+                }
+            }
+
+        } else {
+            if (/[A-Z]/.test(v)) {
+                if (/[a-z][A-Z][a-z\s]/.test(v)) {
+                    return invalidReply(column, v, 'entry cannot contain capital letter unless proper noun');
+                } else {
+                    return;
+                }
+            }
+            if (/^\s/.test(v)) {
+                return invalidReply(column, v, 'entry cannot begin with space');
             }
             return invalidReply(column, v, 'is an invalid entry');
         }
@@ -161,9 +298,11 @@ var gsrel = function (v) {
 
 module.exports = {
 
-    LRB: lrb,
-    Time: time,
-    Gtype: gtype,
-    GSRel: gsrel
+    lrb: lrb,
+    time: time,
+    gtype: gtype,
+    gsrel: gsrel,
+    Key: key,
+    Utts: utts
 
 };
